c929e4d17c73dbac54697fc61be74aa01ab989c2
jatin-rajput563 <jkt3298@gmail.com>
2025-04-29 21:18:14 +0530

    starting-event-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Event from './components/Event'
 import Header from './components/Header'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Header />
+      <Event />
     </>
   )
 }
