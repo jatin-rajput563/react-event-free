6d1f1a743ba856ceff523767e64c340a21755908
jatin-rajput563 <jkt3298@gmail.com>
2025-04-29 23:03:12 +0530

    starting-footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Event from './components/Event'
+import Footer from './components/Footer'
 import Header from './components/Header'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Header />
       <Event />
+      <Footer />
     </>
   )
 }
