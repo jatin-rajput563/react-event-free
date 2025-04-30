346c0d71d68c9e68be6cdaf830ce786c3eb756dc
jatin-rajput563 <jkt3298@gmail.com>
2025-04-30 09:39:56 +0530

    adding-icon.jsx-file

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -5,17 +5,17 @@ import CommonButton from './common/CommonButton'
 const Event = () => {
     return (
         <>
-            <div className="min-h-screen">
+            <div className="pt-[68px] max-sm:pt-[20px] pb-[155px] max-sm:pb-[50px]">
                 <div className="max-w-[1320px] mx-auto max-sm:px-5">
-                    <div className='mt-[68px]'>
+                    <div>
                         <img className='flex justify-center mx-auto' src={blueLine} alt="blue-line" />
-                        <p className='font-bold text-[48px] leading-[100%] m-0 text-center'>Choose your next event</p>
+                        <p className='font-bold text-[48px] max-sm:text-[33px] leading-[100%] m-0 text-center'>Choose your next event</p>
                     </div>
                     <div className="flex flex-wrap -mx-3 flex-row items-center justify-center gap-6 mt-[33px] ">
                         {CARD_DATA.map((item, index) => (
-                            <div className='max-w-[424px] pl-[42px] pr-[37px] pt-[35px] pb-[30px] w-full border rounded-[30px] border-[#00000033] hover:shadow-lg hover:border-0 group transition-all duration-50 ease-in-out'>
+                            <div className='max-w-[424px] pl-[42px] max-sm:pl-[20px] max-sm:pr-[20px] max-sm:pt-[20px] max-sm:pb-[20px] pr-[37px] pt-[35px] pb-[30px] w-full border rounded-[30px] border-[#00000033] hover:shadow-lg hover:border-0 group transition-all duration-50 ease-in-out'>
                                 <img className='w-full max-w-[344px] rounded-[20px] transition-all duration-300 group-hover:rounded-[0px]' src={item.CardImg} alt={item.text} />
-                                <p className='font-semibold text-[24px] leading-[150%] m-0 pt-[21px]'>{item.CardText}</p>
+                                <p className='font-semibold text-[24px] max-sm:text-[20px] leading-[150%] m-0 pt-[21px]'>{item.CardText}</p>
                                 <div className='flex pt-[12px] gap-[8px]'>
                                     <img src={item.CalenderImg} alt="celender-svg" />
                                     <p className='font-normal text-[16px] leading-[150%] opacity-75'>{item.DateText}</p>
